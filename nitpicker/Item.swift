d7448aa2c660457b7297f2838fb84fba9ebe4b59
Vishnu R <vishnu@buymeacoffee.com>
2025-07-07 15:16:38 +0530

    Initial Commit

---
+++ nitpicker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  nitpicker
+//
+//  Created by Vishnu R on 07/07/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ nitpickerTests/nitpickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  nitpickerTests.swift
+//  nitpickerTests
+//
+//  Created by Vishnu R on 07/07/25.
+//
+
+import Testing
+@testable import nitpicker
+
+struct nitpickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
